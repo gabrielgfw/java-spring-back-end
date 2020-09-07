195f89fe7f8e1e80b2b8190c1362eecb67c548f6
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-09-07 20:50:02 -0300

    add delete route for deleting a specific todo id

--- src/main/java/com/gabrielgfw/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/gabrielgfw/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -3,21 +3,40 @@ package com.gabrielgfw.rest.webservices.restfulwebservices.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabrielgfw.rest.webservices.restfulwebservices.todo.Todo;
 
+//CrossOrigin para definir acesso de outro servidor. //
+//Neste caso, do nosso front-end Angular. //
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class TodoResource {
 	
 	@Autowired
 	private TodoHardcodedService todoService;
 	
+	// GET METHOD //
 	@GetMapping("/users/{username}/todos")
 	public List<Todo> getAllTodos(@PathVariable String username) {
 		return todoService.findAll();
 	}
+	
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
+		
+		Todo todo = todoService.deleteById(id);
+		if(todo!=null) {
+			return ResponseEntity.noContent().build();
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
 
 }
