316a692e9e33fd2bca03e4fe5bd3c49b9fe69734
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-09-07 18:41:17 -0300

    add CrossOrigin to allow angular access

--- src/main/java/com/gabrielgfw/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/gabrielgfw/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,12 +1,16 @@
 package com.gabrielgfw.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-// Ã‰ necessÃ¡rio a anotaÃ§Ã£o RestController para definir uma classe Controller. //
+// CrossOrigin para definir acesso de outro servidor. //
+// Neste caso, do nosso front-end Angular. //
+@CrossOrigin(origins="http://localhost:4200")
+//RestController para definir uma classe Controller. //
 @RestController
 public class HelloWorldController {
 	
